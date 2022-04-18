000a89bd17b777996bce8b60fb703ca2b5f6c7fe
Pavel Burov <p.burov@sftpro.ru>
2022-04-18 23:43:07 +0300

    create checkSpam function

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,9 @@
 function checkSpam(str) {
-  // ваш код...
+  let lowerCaseStr = str.toLowerCase()
+  
+  if ((lowerCaseStr.indexOf('1xbet') !== -1) || (lowerCaseStr.indexOf('xxx') !== -1)) {
+  	return true                                               
+	}
+	
+  return false
 }

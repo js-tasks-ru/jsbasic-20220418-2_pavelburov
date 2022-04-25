02a17fc8fbb5afb58503ea202a930cb9355366fa
Pavel Burov <p.burov@sftpro.ru>
2022-04-25 21:32:52 +0300

    create showSalary function

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,6 @@
-function showSalary(users, age) {
-  // ваш код...
+function showSalary(users, maxAge) {
+  return users
+  .filter(({age}) => age <= maxAge )
+  .map(({name, balance}) =>`${name}, ${balance}`)
+  .join('\n')
 }

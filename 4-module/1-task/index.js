44496fee20ba75fb4b07ccc41f01df534d2275c1
Pavel Burov <p.burov@sftpro.ru>
2022-05-02 11:27:00 +0300

    create makeFriendsList

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,12 @@
 function makeFriendsList(friends) {
-  // ваш код...
-}
+  let list = document.createElement("ul");
+
+  for (let friend of friends) { 
+    let item = document.createElement('li');
+		let name = `${friend.firstName} ${friend.lastName}`
+    item.appendChild(document.createTextNode(name));
+    list.appendChild(item);
+  }
+
+  return list
+};

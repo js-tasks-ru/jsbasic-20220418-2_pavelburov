b897b72ef8eda1dd74e1a0e06e8b91a3342f9855
Pavel Burov <p.burov@sftpro.ru>
2022-05-16 23:13:27 +0300

    create initCarousel()

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,33 @@
 function initCarousel() {
-  // ваш код...
+  const carousel = document.querySelector('.carousel__inner');
+  const leftBtn = document.querySelector('.carousel__arrow_left');
+  const rightBtn = document.querySelector('.carousel__arrow_right');
+  const slideWidth = carousel.offsetWidth;
+  let slideNumber = 0;
+
+  const nextSlide = () => {
+    leftBtn.style.display = ''
+
+    slideNumber += 1;
+    let position = slideWidth * slideNumber;
+
+    carousel.style.transform = `translateX(-${position}px)`;
+
+    if (slideNumber === 3) { return rightBtn.style.display = 'none' }
+  };
+
+  const previousSlide = () => {
+    rightBtn.style.display = ''
+    
+    slideNumber -= 1;
+    let position = slideWidth * slideNumber;
+
+    carousel.style.transform = `translateX(-${position}px)`;
+    
+    if (slideNumber === 0) { return leftBtn.style.display = 'none' }
+  };
+
+  leftBtn.style.display = 'none';
+  rightBtn.addEventListener('click', nextSlide);
+  leftBtn.addEventListener('click', previousSlide);
 }

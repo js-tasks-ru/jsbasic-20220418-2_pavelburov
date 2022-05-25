ffad1b0e58449ff3ea30b7460a51e258cf6668e5
Pavel Burov <p.burov@sftpro.ru>
2022-05-25 10:08:32 +0300

    add updateFilter() to ProductGrid

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -4,6 +4,68 @@ import ProductCard from '../../6-module/2-task/index.js';
 export default class ProductGrid {
   constructor(products) {
     this.products = products;
-    this.filters = {};
+    this.filters = {
+                      nuts: undefined,
+                      vegeterian: undefined,
+                      spiciness: 4,
+                      category: ''
+                    };
+    this.elem = createElement(` <div class="products-grid">
+                                  <div class="products-grid__inner">
+                                    <!--ВОТ ТУТ БУДУТ КАРТОЧКИ ТОВАРОВ-->
+                                  </div>
+                                </div>
+                              `)
+
+    this.render(products);
+  }
+
+  render(products) {
+    const productGridInner = this.elem.querySelector('.products-grid__inner');
+
+    while (productGridInner.firstChild) {
+      productGridInner.removeChild(productGridInner.lastChild);
+    }
+
+    products.forEach(product => {
+      const productCard = new ProductCard(product)
+      
+      productGridInner.appendChild(productCard.elem)
+    });
+  }
+
+  updateFilter(filters) {
+    if (filters.noNuts !== undefined) { this.filters.nuts = !filters.noNuts }
+    if (filters.vegeterianOnly !== undefined) { this.filters.vegeterian = filters.vegeterianOnly  }
+    if (filters.maxSpiciness !== undefined) { this.filters.spiciness = filters.maxSpiciness }
+    if (filters.category !== undefined) { this.filters.category = filters.category }
+
+
+    let nutsFiltered = this.products;
+    if (this.filters.nuts === false) {
+      nutsFiltered = this.products.filter(product => {
+        return (product.nuts === this.filters.nuts || product.nuts === undefined)
+      });
+    }
+
+    let vegeteiranFiltered = nutsFiltered;
+    if (this.filters.vegeterian) {
+      vegeteiranFiltered = nutsFiltered.filter(product => {
+        return (product.vegeterian === this.filters.vegeterian)
+      });
+    }
+
+    let spicinessFiltered = vegeteiranFiltered.filter(product => {
+      return (product.spiciness <= this.filters.spiciness)
+    });
+
+    let categoryFiltered = spicinessFiltered;
+    if (this.filters.category) {
+      categoryFiltered = spicinessFiltered.filter(product => {
+        return (product.category === this.filters.category)
+      });
+    }
+
+    this.render(categoryFiltered)
   }
 }
